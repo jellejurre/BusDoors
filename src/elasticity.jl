08cc1f6b8f4b81c938420a3d759ad61e361b7c54
Jelle Groenendijk <j.j.h.groenendijk@student.tudelft.nl>
2022-12-12 20:57:02 +0100

    Add proper force vectorisation to door

--- src/elasticity.jl
+++ src/elasticity.jl
@@ -1,55 +1,26 @@
 using Gridap
 using GridapGmsh
 import LinearAlgebra: norm
-model = GmshDiscreteModel("./geometry.msh")
-
-# order = 1
-
-# reffe = ReferenceFE(lagrangian,VectorValue{3,Float64},order)
-# V0 = TestFESpace(model,reffe;
-#   conformity=:H1,
-#   dirichlet_tags="DirichletEdges",
-#   dirichlet_masks=[(true,false,false), (true,true,true)])
-# g1(x) = VectorValue(0.005,0.0,0.0)
-# g2(x) = VectorValue(0.0,0.0,0.0)
-# U = TrialFESpace(V0,[g1,g2])
-# const E = 70.0e9
-# const ν = 0.33
-# const λ = (E*ν)/((1+ν)*(1-2*ν))
-# const μ = E/(2*(1+ν))
-# σ(ε) = λ*tr(ε)*one(ε) + 2*μ*ε
-# degree = 2*order
-# Ω = Triangulation(model)
-# dΩ = Measure(Ω,degree)
-# a(u,v) = ∫( ε(v) ⊙ (σ∘ε(u)) )*dΩ
-# l(v) = 0
-# op = AffineFEOperator(a,l,U,V0)
-# uh = solve(op)
-# writevtk(Ω,"results_elasticity",cellfields=["uh"=>uh,"epsi"=>ε(uh),"sigma"=>σ∘ε(uh)])
-
+import Statistics: mean
 using Gridap.Geometry
+
+# Setup model
+model = GmshDiscreteModel("./geometry.msh")
 labels = get_face_labeling(model)
 dimension = 3
+order = 1
+
+# Setup fields
+reffe = ReferenceFE(lagrangian,VectorValue{3,Float64},order)
+V0 = TestFESpace(model,reffe;conformity=:H1)
+U = TrialFESpace(V0)
+
+# Setup tags
 tags = get_face_tag(labels,dimension)
 const glass_tag = get_tag_from_name(labels,"Glass")
 const aluminium_tag = get_tag_from_name(labels,"Aluminium")
 
-order = 1
-
-reffe = ReferenceFE(lagrangian,VectorValue{3,Float64},order)
-# V0 = TestFESpace(model,reffe;
-#   conformity=:H1,
-#   dirichlet_tags="DirichletEdges",
-#   dirichlet_masks=[(true,false,false), (true,true,true)]
-#   )
-V0 = TestFESpace(model,reffe;
-  conformity=:H1
-  )
-# g1(x) = VectorValue(0.005,0.0,0.0)
-# g2(x) = VectorValue(0.0,0.0,0.0)
-# U = TrialFESpace(V0,[g1,g2])
-U = TrialFESpace(V0)
-
+# Setup lamé parameters
 function lame_parameters(E,ν)
   λ = (E*ν)/((1+ν)*(1-2*ν))
   μ = E/(2*(1+ν))
@@ -72,32 +43,38 @@ function σ_bimat(ε,tag)
   end
 end
 
-sources = [(10, 1, VectorValue(0.6,1,0))]
+# Setup forces
+sources = [(10, 1, VectorValue(0,0,0), VectorValue(0, 0, 1))]
 
-function get_force(x, tag)
-  # if (tag != aluminium_tag)
-  #   return 0
-  # end
-  sum_forces = 0
+function bell_curve(mean, deviation, x)
+  return 1/(deviation * sqrt(2 * π)) * ℯ^(-norm(x - mean)/(2 * deviation^2))
+end
+
+function get_force(x)
+  nodeAverage = mean(x) #Average required because we have the coordinates of the four corners of the cell
+  sum_forces = VectorValue(0, 0, 0)
   for source in sources
-    val = source[1] * ℯ^(-(0.1 + source[2] * norm(source[3] - x)))
-    sum_forces += val
+    amplitude = source[1] * bell_curve(source[3], source[2], nodeAverage)
+    force_val = amplitude * source[4]
+    sum_forces += force_val
   end
   return sum_forces
 end
 
+forces = map((cell) -> get_force(cell), get_cell_coordinates(model))
+
+# Set up equation
 σ(ε) = λ*tr(ε)*one(ε) + 2*μ*ε
 degree = 2*order
 Ω = Triangulation(model)
 dΩ = Measure(Ω,degree)
-
-Γ = BoundaryTriangulation(model)
-dΓ = Measure(Γ,degree)
-
 a(u,v) = ∫( ε(v) ⊙ (σ_bimat∘(ε(u),tags)) )*dΩ
-l(v) = ∫( get_force∘(v, tags) )*dΓ
+l(v) = ∫( v⋅forces )*dΩ
 
+# Perform equation
 op = AffineFEOperator(a,l,U,V0)
 uh = solve(op)
-writevtk(Ω,"results_bimat",cellfields=
-  ["uh"=>uh,"epsi"=>ε(uh),"sigma"=>σ_bimat∘(ε(uh),tags)])
+
+# Write output
+writevtk(Ω,"results",cellfields=
+  ["force"=>forces, "uh"=>uh,"epsi"=>ε(uh),"sigma"=>σ_bimat∘(ε(uh),tags)])
